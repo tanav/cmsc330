2df8272fcc495caa3a53feccd605d68fd383ff2c
tanav <itstanav@gmail.com>
2023-06-28 00:33:44 -0400

    Uploaded OCaml Warmup

---
+++ OCaml Warmup/src/basics.ml
@@ -0,0 +1,52 @@
+open Funs
+
+(***********************************)
+(* Part 1: Non-Recursive Functions *)
+(***********************************)
+
+let rev_tup (a, b, c) = failwith "unimplemented"
+
+let is_even x = failwith "unimplemented"
+
+let area (a, b) (x, y) = failwith "unimplemented"
+
+(*******************************)
+(* Part 2: Recursive Functions *)
+(*******************************)
+
+let rec fibonacci n = failwith "unimplemented"
+
+let rec pow x p = failwith "unimplemented"
+
+let rec is_prime x = failwith "unimplemented"
+
+let rec maxFuncChain init funcs = failwith "unimplemented"
+
+(*****************)
+(* Part 3: Lists *)
+(*****************)
+
+let reverse lst = failwith "unimplemented"
+
+let rec merge lst1 lst2 = failwith "unimplemented"
+
+let rec is_palindrome lst = failwith "unimplemented"
+
+      
+let jumping_tuples lst1 lst2 = failwith "unimplemented"
+
+let rec flatten lst = failwith "unimplemented"
+
+let rec square_primes lst = failwith "unimplemented"
+
+let rec partition p lst = failwith "unimplemented"
+
+(*****************)
+(* Part 4: HOF *)
+(*****************)
+
+let is_present lst x = failwith "unimplemented"
+
+let count_occ lst target = failwith "unimplemented"
+
+let uniq lst = failwith "unimplemented"
