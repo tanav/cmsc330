3381387e8069ce6db954b042ec537528d30d8f97
tanav <itstanav@gmail.com>
2023-06-28 00:32:47 -0400

    Uploaded Translator

---
+++ Translator/src/translate.rb
@@ -0,0 +1,40 @@
+class Translator
+
+    def initialize(words_file, grammar_file)
+  
+    end
+
+    # part 1
+  
+    def updateLexicon(inputfile)
+  
+    end
+  
+    def updateGrammar(inputfile)
+  
+    end
+
+    # part 2
+  
+    def generateSentence(language, struct)
+  
+    end
+  
+    def checkGrammar(sentence, language)
+  
+    end
+  
+    def changeGrammar(sentence, struct1, struct2)
+  
+    end
+
+    # part 3
+  
+    def changeLanguage(sentence, language1, language2)
+  
+    end
+  
+    def translate(sentence, language1, language2)
+  
+    end
+  end  
